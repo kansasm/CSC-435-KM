4d237af1ce2f97710ef6e2c296d38fa872556831
kansas martinez <martinezk01@lsus.edu>
2018-02-16 23:24:57 -0600

    Assignment 4 branch for registration form.

---
+++ WebApps1/WebApps1/App_Data/dbo.Table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [FirstName] VARCHAR(50) NOT NULL, 
+    [LastName] VARCHAR(50) NOT NULL, 
+    [Email] VARCHAR(50) NOT NULL, 
+    [Password] VARCHAR(50) NOT NULL, 
+    [Gender] VARCHAR(50) NOT NULL
+)
---
+++ WebApps1/WebApps1/Controllers/RegistrationController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApps1.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        // GET: Registration
+        public ActionResult Register(Table T)
+        {
+            return View(T);
+        }
+
+    }
+}
